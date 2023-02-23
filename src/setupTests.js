265a282c138cfe1fed5dcb731e59757fd09fd211
Bradley T. Landis <bradleylandis@bradleylandis.com>
2023-02-23 10:28:48 -0500

    replace fetch, Request, Response, AbortController and TextEncoder/TextDecoder for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,16 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import {
+  TextEncoder as NodeTextEncoder,
+  TextDecoder as NodeTextDecoder,
+} from "util";
+import { fetch, Request, Response } from "@remix-run/web-fetch";
+import { AbortController as NodeAbortController } from "abort-controller";
+
+globalThis.fetch = fetch;
+globalThis.Request = Request;
+globalThis.Response = Response;
+globalThis.AbortController = NodeAbortController;
+globalThis.TextEncoder = NodeTextEncoder;
+globalThis.TextDecoder = NodeTextDecoder;
